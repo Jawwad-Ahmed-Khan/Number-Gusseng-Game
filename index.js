5abc789dbee2ab246b96c7ff98d12fc6e6a82fef
Jawwad-Ahmed-Khan <jawwadahmedkhan63@gmail.com>
2024-04-11 09:54:18 +0500

    I made This Project without any tutorials seen before it.

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+let Random_number = Math.ceil(1.0);
+console.log(`Seil Random number:${Random_number}`);
+while (1) {
+    Random_number = Math.floor(Math.random() * 10) + 1;
+    console.log(`Random number:${Random_number}`);
+    let guess = await inquirer.prompt([{ message: "Enter My gues", type: "number", name: "guess" }]);
+    if (guess.guess === Random_number) {
+        console.log("You Win");
+        break;
+    }
+    else {
+        console.log("Try again");
+    }
+}
